d2b6f9eedfaee89a5f74dfce222ff8107f38de28
samtogo407 <mohammedabdulhady407@gmail.com>
2024-09-22 19:01:16 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function redirectToLinkedIn() {
+  window.location.href = "https://pl.linkedin.com/in/mohamed-elsonbaty-411152121";
+}
+
+function redirectToGitHub() {
+  window.location.href = "https://github.com/samtogo407";
+}
+
+function redirectToDockerHub() {
+  window.location.href = "https://hub.docker.com/u/togo407";
+}
